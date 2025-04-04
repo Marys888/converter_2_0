0dd6418a881c4493a522a286d7a3b9f46f8312ea
Marys888 <severovamaha1999@gmail.com>
2025-04-04 14:04:36 +0200

    Add miles to kilometers conversion

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,20 @@
 package app;
+import java.util.Scanner;
 
 public class Main {
+    private static final double KMS_IN_MILE = 1.60934;
+
     public static void main(String[] args) {
         System.out.println("My SSH application..");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number in miles: ");
+        double miles = scanner.nextDouble();
+        System.out.println("Kilometers: " + " " + milesToKmsConverter(miles));
+        scanner.close();
+    }
+
+    private static double milesToKmsConverter(double miles) {
+        return miles * KMS_IN_MILE;
+
     }
 }
